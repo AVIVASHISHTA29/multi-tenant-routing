dd1b8a8589fb44fea5796f80cb89a2d1492c4b59
AviVashishta29 <avivashishta@gmail.com>
2024-08-08 18:22:14 +0530

    added assets

--- app/pg/page.tsx
+++ app/pg/page.tsx
@@ -2,7 +2,7 @@
 export default function BlogPage() {
   return (
     <div className="min-h-screen bg-blue-100 flex items-center justify-center">
-      <h1 className="text-4xl font-bold">Welcome to the Blog Subdomain</h1>
+      <h1 className="text-4xl font-bold">Welcome to the PG Subdomain</h1>
     </div>
   );
 }
